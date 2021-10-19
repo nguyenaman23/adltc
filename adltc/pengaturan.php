baa46cb6cd72d57270c8b89ffdb067f887069e48
nguyenaman23 <91823952+nguyenaman23@users.noreply.github.com>
2021-10-19 20:42:06 +0700

    Add files via upload

---
+++ adltc/pengaturan.php
@@ -0,0 +1,7 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Linux; Android 7.1.2; SM-T285) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Safari/537.36";
+
+//ikuti cara ambil cookie di video
+
+$ssid_ltc = "PHPSESSID=eprhsticia9gqjk3m3fmd2otv6";
